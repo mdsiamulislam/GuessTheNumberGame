3e24b4e8d784eae418ad7443737e0f61e936eff8
fardinkamal62 <fardinkamal62@protonmail.ch>
2023-09-16 10:08:01 +0600

    fixed

--- number guessing game.c
+++ number guessing game.c
@@ -1,33 +1,49 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
 
-int main(){
+int main()
+{
     srand(time(NULL));
     int randomNum = rand();
-    int a,b;
-   int lenth = sizeof(randomNum);
-   a=1 * pow(10, lenth);
-   int result = 0;
-        for (int i = 0; i < lenth+1; i++) {
-            result = result * 10 + 9;
-        }
-        b=result;
-    int i,number=randomNum;
-     printf("Enter a number %d to %d : ",a,b);
 
-    while(i<1){
-         int x;
-        scanf("%d",&x);
-        if(x<number){
-             printf("Your Number is small. Try Again : ");
-             i=0;
-        }else if(x>number){
-            printf("Your Number is Big. Try Again : ");
-            i=0;
-        }else{
+    char randomNumString[100];
+    sprintf(randomNumString, "%d", randomNum);
+    int length = strlen(randomNumString);
+
+    printf("\n\n\n\n\n\t\t\t\t\t-----> Welcome to Number Guessing Game <-----\n\n\n\n\n\t\t\t\t\t");
+    int a, b;
+
+    a = pow(10, length);
+    int result = 0;
+    for (int i = 0; i < length + 1; i++)
+    {
+        result = result * 10 + 9;
+    }
+    b = result;
+    int i, number = randomNum;
+    printf("Enter a number %d to %d : ", a, b);
+
+    while (i < 1)
+    {
+        int x;
+        scanf("%d", &x);
+        if (x < number)
+        {
+            printf("\t\t\t\t\tYour Number is small. Try Again : ");
+            i = 0;
+        }
+        else if (x > number)
+        {
+            printf("\t\t\t\t\tYour Number is Big. Try Again : ");
+            i = 0;
+        }
+        else
+        {
             printf("\n\n\n\n\n\t\t\t\t\t-----> Congratulation You Win <-----\n\n\n\n\n\t\t\t\t\t");
-            i=2;
+            i = 2;
         }
     }
 }
